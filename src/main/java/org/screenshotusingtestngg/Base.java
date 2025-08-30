f704ae7383219f06ea402851809c1e9c08a485aa
Satyam67Qa <satyamsingh28389@gmail.com>
2025-08-31 01:18:46 +0530

    Screenshot using testng1

---
+++ src/main/java/org/screenshotusingtestngg/Base.java
@@ -0,0 +1,27 @@
+package org.screenshotusingtestngg;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+
+public class Base {
+    public static WebDriver driver;
+    public static void initilaization(){
+        driver= new ChromeDriver();
+        driver.get("https://awesomeqa.com/webtable.html");
+    }
+
+        public void failed(String testMethodName){
+
+         File fileName = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+         try {
+             FileUtils.copyFile(fileName,new File("C:/Workspace/screenShot/src/main/java/ScreenShot"+testMethodName+"_"+".jpg"));
+         }catch (Exception e){
+             e.printStackTrace();
+         }
+        }
+}
---
+++ src/test/java/org/screenshotusingtestngg/ScreenShotTest.java
@@ -0,0 +1,33 @@
+package org.screenshotusingtestngg;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+@Listeners(CustomListener.class)
+public class ScreenShotTest extends Base{
+
+    @BeforeMethod
+    public void setUp(){
+        initilaization();
+    }
+
+    @Test
+    public void loginTest(){
+        Assert.assertEquals(false,true);
+    }
+    @Test
+    public void searchTest(){
+        Assert.assertEquals(false,true);
+    }
+    @Test
+    public void createDealTest(){
+        Assert.assertEquals(false,true);
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+}
